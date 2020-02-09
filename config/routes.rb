2f2bc2e23b2a4f6d82d7379eab2d447f8bdb086e
L.Carquet <laetitiacarquet@gmail.com>
2020-02-09 13:41:35 +0100

    response with url capture pattern

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/hello' => 'application#say_hello'
+  get 'hello/:name' => 'application#say_hello'
 end
