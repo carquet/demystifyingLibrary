9e5dda80defaa2cd9a0b286167d2dd3aabb61434
L.Carquet <laetitiacarquet@gmail.com>
2020-02-09 14:27:01 +0100

    show one book to the user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,14 @@ class ApplicationController < ActionController::Base
     	render 'application/list_books', locals: { books: books }
 	end
 
+	def show_book
+		connection = SQLite3::Database.new 'db/development.sqlite3'
+    	connection.results_as_hash = true
+
+    	book = connection.execute("SELECT * FROM books WHERE books.id = ? LIMIT 1", params[:id]).first
+    	
+    	render 'application/show_book', locals: { book: book }
+	end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/hello' => 'application#say_hello'
   get 'hello/:name' => 'application#say_hello'
-  get 'list_books'  => 'application#list_books'
+  get '/list_books'  => 'application#list_books'
+  get '/show_book/:id'   => 'application#show_book'
 end
