dadb9b5742b03ffade83aa4c9755ffd941f0b2b1
L.Carquet <laetitiacarquet@gmail.com>
2020-02-09 14:30:54 +0100

    extract db logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,24 @@
 class ApplicationController < ActionController::Base
 
 	def list_books
-		connection = SQLite3::Database.new 'db/development.sqlite3'
-    	connection.results_as_hash = true
-
+		
     	books = connection.execute("SELECT * FROM books")
 
     	render 'application/list_books', locals: { books: books }
 	end
 
 	def show_book
-		connection = SQLite3::Database.new 'db/development.sqlite3'
-    	connection.results_as_hash = true
-
+		
     	book = connection.execute("SELECT * FROM books WHERE books.id = ? LIMIT 1", params[:id]).first
     	
     	render 'application/show_book', locals: { book: book }
 	end
+
+	def connection
+	    db_connection = SQLite3::Database.new 'db/development.sqlite3'
+	    db_connection.results_as_hash = true
+	    db_connection
+  	end
 
 
 end
